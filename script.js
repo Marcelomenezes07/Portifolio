791b85cccb3d10cec694077821cf0528740cd9e3
Marcelo Menezes <66328929+Marcelomenezes07@users.noreply.github.com>
2025-06-09 17:46:28 -0300

    adicionando o arquivo

---
+++ script.js
@@ -0,0 +1,18 @@
+function navigate(pageId) {
+  // Oculta todas as páginas
+  const pages = document.querySelectorAll('.page');
+  pages.forEach(page => {
+    page.classList.remove('active');
+  });
+
+  // Mostra a página escolhida
+  const selectedPage = document.getElementById(pageId);
+  if (selectedPage) {
+    selectedPage.classList.add('active');
+  }
+}
+
+// Mostra a página "home" ao carregar o site
+window.onload = function () {
+  navigate('contato');
+};
